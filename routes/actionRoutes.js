2cd50b70d74a8ca1ca2646a6759b43357b74f4fa
Jacob Henry <jacobiswinning@gmail.com>
2018-06-01 14:45:14 -0600

    added status codes and methods for crud Actions only

---
+++ routes/actionRoutes.js
@@ -0,0 +1,75 @@
+const express = require('express')
+
+const router = express.Router()
+
+const db = require('../data/helpers/actionModel.js')
+
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(error => {
+      res.status(404).json(error)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const {
+    id
+  } = req.params
+
+  db
+    .get(id)
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(error => {
+      res.status(500).json(console.error('Error updating action', error))
+    })
+})
+
+router.post('/', (req, res) => {
+  const action = req.body
+
+  db
+    .insert(action)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(500).json(console.error('Error updating action', error))
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const {
+    id
+  } = req.params
+
+  db
+    .remove(id)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(500).json(console.error('Error updating action', error))
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const changes = req.body
+
+  db
+    .update(id, changes)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(500).json(console.error('Error updating action', error))
+    })
+})
+
+module.exports = router
