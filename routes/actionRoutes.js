582758fabe2165f3d04194d5e2b5fee7380d9e61
Jacob Henry <jacobiswinning@gmail.com>
2018-06-01 16:52:12 -0600

    trying to get max length 120 to work

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -41,7 +41,23 @@ router.post('/', (req, res) => {
     .catch(error => {
       res.status(500).json(console.error('Error updating action', error))
     });
+  // if (!action.description) {
+  //   res.status(400).json({
+  //     error: 'description is Required'
+  //   });
+  // } else if (action.description.length > 120) {
+  //   res.status(400).json({
+  //     error: 'The Max length is 120 characters'
+  //   });
+  // } else {
+  //   res.status(500).json(error);
+  // }
 });
+
+
+
+
+
 
 router.delete('/:id', (req, res) => {
   const {
@@ -59,18 +75,18 @@ router.delete('/:id', (req, res) => {
 });
 
 
-// router.put('/:id', (req, res) => {
-//   const id = req.params.id
-//   const changes = req.body
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const changes = req.body
 
-//   db
-//     .update(id, changes)
-//     .then(response => {
-//       res.status(200).json(response)
-//     })
-//     .catch(error => {
-//       res.status(500).json(console.error('Error updating action', error))
-//     })
-// })
+  db
+    .update(id, changes)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(500).json(console.error('Error updating action', error))
+    })
+})
 
 module.exports = router
